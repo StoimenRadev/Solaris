fcab839ab712185b70ca533b40aa3c6bb5c5bb8a
StoimenRadev <radevstoimen@gmail.com>
2025-12-07 10:38:32 +0200

    Bug fix
    Updated the FixUI script because some icons of the buttons were not showing.

--- Assets/Scripts/InteractMenu/HamburgerMenu/FixedUI.cs
+++ Assets/Scripts/InteractMenu/HamburgerMenu/FixedUI.cs
@@ -1,21 +1,38 @@
 using UnityEngine;
 
+[ExecuteAlways]
 public class FixedUI : MonoBehaviour
 {
-    private Vector3 startPosition;
-    private Quaternion startRotation;
+    private RectTransform rectTransform;
+    private Vector2 startAnchoredPosition;
+    private Quaternion startLocalRotation;
 
-    void Start()
+    void Awake()
     {
-        // Store the initial position and rotation
-        startPosition = transform.position;
-        startRotation = transform.rotation;
+        rectTransform = GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            startAnchoredPosition = rectTransform.anchoredPosition;
+            startLocalRotation = rectTransform.localRotation;
+        }
+        else
+        {
+            Debug.LogWarning("FixedUI attached to a non-UI object. Consider using the original world position version.");
+        }
     }
 
     void LateUpdate()
     {
-        // Reset position and rotation every frame
-        transform.position = startPosition;
-        transform.rotation = startRotation;
+        if (rectTransform != null)
+        {
+            rectTransform.anchoredPosition = startAnchoredPosition;
+            rectTransform.localRotation = startLocalRotation;
+        }
+        else
+        {
+            // Fallback for non-UI objects
+            transform.position = startAnchoredPosition;
+            transform.rotation = startLocalRotation;
+        }
     }
 }
