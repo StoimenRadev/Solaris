2850d2ae8222c3049b29cc3d3aebdf87e8e7793e
StoimenRadev <radevstoimen@gmail.com>
2026-02-16 16:03:27 +0200

    Hover Text Improvance
    I improved the hover for the time buttons in the TimeLine, by setting the default text's font size to be smaller and to become bigger when it shows the buttons.

--- Assets/Scripts/Ui/HoverShowButtons.cs
+++ Assets/Scripts/Ui/HoverShowButtons.cs
@@ -1,15 +1,23 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using System.Collections;
 
 public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public RectTransform textTransform;       // The text that moves up
-    public CanvasGroup[] buttons;             // The buttons that fade in/out
-    public float fadeDuration = 0.5f;         // Buttons fade speed
-    public float moveDuration = 0.5f;         // Text move speed
-    public float moveAmount = 50f;            // How far text moves up
-    public float hideDelay = 5f;              // Delay before buttons hide
+    public RectTransform textTransform;
+    public TextMeshProUGUI textComponent;
+
+    public CanvasGroup[] buttons;
+
+    public float fadeDuration = 0.5f;
+    public float moveDuration = 0.5f;
+    public float hideDelay = 5f;
+
+    public float moveAmount = 50f;
+
+    public float smallFontSize = 28f;
+    public float bigFontSize = 40f;
 
     private Vector3 textOriginalPos;
     private Coroutine fadeCoroutine;
@@ -19,8 +27,8 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
     void Start()
     {
         textOriginalPos = textTransform.localPosition;
+        textComponent.fontSize = smallFontSize;
 
-        // Hide buttons at start
         foreach (var btn in buttons)
         {
             btn.alpha = 0;
@@ -37,11 +45,7 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
             hideCoroutine = null;
         }
 
-        // Move text up
-        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
-        moveCoroutine = StartCoroutine(MoveText(textTransform.localPosition, textOriginalPos + Vector3.up * moveAmount));
-
-        // Show buttons
+        AnimateText(textOriginalPos + Vector3.up * moveAmount, bigFontSize);
         StartFade(1);
     }
 
@@ -54,17 +58,45 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
     {
         yield return new WaitForSeconds(hideDelay);
 
-        // Move text back down
-        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
-        moveCoroutine = StartCoroutine(MoveText(textTransform.localPosition, textOriginalPos));
-
-        // Hide buttons
+        AnimateText(textOriginalPos, smallFontSize);
         StartFade(0);
+    }
+
+    void AnimateText(Vector3 targetPos, float targetFontSize)
+    {
+        if (moveCoroutine != null)
+            StopCoroutine(moveCoroutine);
+
+        moveCoroutine = StartCoroutine(MoveAndResize(targetPos, targetFontSize));
+    }
+
+    IEnumerator MoveAndResize(Vector3 targetPos, float targetFontSize)
+    {
+        float time = 0;
+
+        Vector3 startPos = textTransform.localPosition;
+        float startFont = textComponent.fontSize;
+
+        while (time < moveDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / moveDuration;
+
+            textTransform.localPosition = Vector3.Lerp(startPos, targetPos, t);
+            textComponent.fontSize = Mathf.Lerp(startFont, targetFontSize, t);
+
+            yield return null;
+        }
+
+        textTransform.localPosition = targetPos;
+        textComponent.fontSize = targetFontSize;
     }
 
     void StartFade(float targetAlpha)
     {
-        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+
         fadeCoroutine = StartCoroutine(FadeButtons(targetAlpha));
     }
 
@@ -72,9 +104,10 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
     {
         float time = 0;
         float[] startAlphas = new float[buttons.Length];
-        for (int i = 0; i < buttons.Length; i++) startAlphas[i] = buttons[i].alpha;
 
-        // Enable interaction immediately when fading in
+        for (int i = 0; i < buttons.Length; i++)
+            startAlphas[i] = buttons[i].alpha;
+
         if (targetAlpha == 1)
         {
             foreach (var btn in buttons)
@@ -87,15 +120,16 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
         while (time < fadeDuration)
         {
             time += Time.deltaTime;
+
             for (int i = 0; i < buttons.Length; i++)
                 buttons[i].alpha = Mathf.Lerp(startAlphas[i], targetAlpha, time / fadeDuration);
+
             yield return null;
         }
 
         for (int i = 0; i < buttons.Length; i++)
             buttons[i].alpha = targetAlpha;
 
-        // Disable interaction after fade out
         if (targetAlpha == 0)
         {
             foreach (var btn in buttons)
@@ -104,17 +138,5 @@ public class HoverShowButtons : MonoBehaviour, IPointerEnterHandler, IPointerExi
                 btn.blocksRaycasts = false;
             }
         }
-    }
-
-    IEnumerator MoveText(Vector3 from, Vector3 to)
-    {
-        float time = 0;
-        while (time < moveDuration)
-        {
-            time += Time.deltaTime;
-            textTransform.localPosition = Vector3.Lerp(from, to, time / moveDuration);
-            yield return null;
-        }
-        textTransform.localPosition = to;
     }
 }
